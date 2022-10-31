8048f0473b15bdaa63a263f53fc2ecc9851f7b19
devbhura <devbhura@gmail.com>
2022-10-30 19:29:51 -0500

    Updates

--- sim_pkg/firefly.py
+++ sim_pkg/firefly.py
@@ -3,72 +3,75 @@ import random
 
 def usr(robot):
 
-    T = random.randint(600,800)
+    T = random.randint(800,1300)
     print(T)
     while True:        
         id = robot.id
 
-        if id == 1:
+        if id == 5:
             robot.set_led(0,100,0)
             robot.delay(100)
             curr_time = robot.get_clock()
             curr_time = round(curr_time, 2)
             msg = "time:"+ str(curr_time)+';'
-            robot.send_msg("led:(0,100,0);delay:1000;"+msg)
+            robot.send_msg("led:(0,100,0);delay:500;"+msg)
             robot.set_led(0,0,0)
-            robot.delay(400)
+            robot.delay(500)
 
         else:
             val = robot.recv_msg(clear=True)
-            if len(val[0]) > 1:
-                txt = val[0]
-                # print(val)
-                sub_str = 'delay'
-                res = [i for i in range(len(txt)) if txt.startswith(sub_str, i)]
+            if len(val)>0:
+                if len(val[0]) > 1:
+                    txt = val[0]
+                    # print(val)
+                    sub_str = 'delay'
+                    res = [i for i in range(len(txt)) if txt.startswith(sub_str, i)]
 
-                # print(txt)
-                # Get the delay
-                ind = res[-1]
-                txt = txt[ind:]
-                # msg_time = "time:"+ str(curr_time)+';'
-                # r, g, b = (0, 100, 0)
-                # msg = "led:("+ str(r)+","+str(g)+","+str(b)+");" + txt
-                # robot.send_msg(msg)
-                # print(ind)
-                
-                
-                # print(txt)
-                ind_delim = txt.index(";")
-                # print(ind_delim)
-                delay_val = txt[6:ind_delim]
-                # print("Delay_val:",delay_val)
-                delay_val = float(delay_val)
-                
-                # Get the time 
-                ind = txt.index("time")
-                ind_delim = ind +1 + txt[ind+1:].index(";")
-                time_val = txt[ind+5:ind_delim]
-                time_val = float(time_val)
+                    # print(txt)
+                    # Get the delay
+                    ind = res[-1]
+                    txt = txt[ind:]
+                    # msg_time = "time:"+ str(curr_time)+';'
+                    # r, g, b = (0, 100, 0)
+                    # msg = "led:("+ str(r)+","+str(g)+","+str(b)+");" + txt
+                    # robot.send_msg(msg)
+                    # print(ind)
+                    
+                    
+                    # print(txt)
+                    ind_delim = txt.index(";")
+                    # print(ind_delim)
+                    delay_val = txt[6:ind_delim]
+                    # print("Delay_val:",delay_val)
+                    delay_val = float(delay_val)
+                    
+                    # Get the time 
+                    ind = txt.index("time")
+                    ind_delim = ind +1 + txt[ind+1:].index(";")
+                    time_val = txt[ind+5:ind_delim]
+                    time_val = float(time_val)
+                    curr_time = robot.get_clock()
+                    
+                    next_time = time_val + delay_val/1000
+                    # print("Next time:",next_time)
+                    T = delay_val
+                    del_Sleep = next_time - curr_time -0.05
+                    # print("Del Sleep:",del_Sleep)
+                    if del_Sleep> 0:
+                        robot.delay(int(del_Sleep))
+                    
+                    
+                    
+                r, g, b = (0, 100, 0)
+                robot.set_led(r,g,b)
+                robot.delay(100)
+                T = int(T)
                 curr_time = robot.get_clock()
-                
-                next_time = time_val + delay_val/1000
-                # print("Next time:",next_time)
-                T = delay_val
-                del_Sleep = next_time - curr_time -0.05
-                # print("Del Sleep:",del_Sleep)
-                if del_Sleep> 0:
-                    time.sleep(del_Sleep)
-                
-                
-                
-            r, g, b = (0, 100, 0)
-            robot.set_led(r,g,b)
-            robot.delay(100)
-            T = int(T)
-            curr_time = robot.get_clock()
-            msg_time = "time:"+ str(curr_time)+';'
-            msg = "led:("+ str(r)+","+str(g)+","+str(b)+");" + "delay:"+ str(T)+";" + msg_time
-            robot.send_msg(msg)
-            robot.set_led(0,0,0)
-            robot.delay(T)
+                msg_time = "time:"+ str(curr_time)+';'
+                msg = "led:("+ str(r)+","+str(g)+","+str(b)+");" + "delay:"+ str(T)+";" + msg_time
+                robot.send_msg(msg)
+                robot.set_led(0,0,0)
+                robot.delay(T)
+            else: 
+                continue
         
